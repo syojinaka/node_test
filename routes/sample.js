7bf1e73f7db01e773089feb75eb4f2e55245909e
仲野 耀路 <88647@ad.exa-corp.co.jp>
2019-09-10 10:11:55 +0900

    add file sample

---
+++ routes/sample.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    var param = {"name":"XXXX"};
+    res.send(param);
+});
+
+module.exports = router;
